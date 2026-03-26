58cc54c09842367aecec40ef1a91c5130136b217
sumantgigacars <sumant.shah@gigacars.com>
2026-03-26 16:06:04 +0530

    lec 76

---
+++ 76. (Exersice 8) Merge the pdf.py
@@ -0,0 +1,8 @@
+# Merge the PDF 
+
+'''
+Question: 8
+o Write a program to manipulate pdf files using pyPDF. Your progress
+should be able to merge multiple pdf files into a single pdf. You 
+are welcome to add more functionalities.
+'''
